4c0cf0ac0291ff2bf0586261102cea19f3414404
Nickson Kipruto Rop <nickyrutto@gmail.com>
2024-04-30 07:47:04 +0300

    user registration form

--- account/forms.py
+++ account/forms.py
@@ -19,6 +19,6 @@ class UserRegistrationForm(forms.ModelForm):
 
     def clean_password2(self):
         cd = self.cleaned_data
-        if cd['password'] != cd['password']:
+        if cd['password'] != cd['password2']:
             raise forms.ValidationError('Passwords don\'t match.')
         return cd['password2']
