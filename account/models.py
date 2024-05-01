31834b4f665d4e48ed7c2da04be8f04f82637a63
Nickson Kipruto Rop <nickyrutto@gmail.com>
2024-05-01 18:29:01 +0300

    add website, twitter, bio and github fields

--- account/models.py
+++ account/models.py
@@ -7,6 +7,10 @@ class Profile(models.Model):
     This class extends the User model and provides additional fields. Has a one-to-one relationship with the Django User model.
     """
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    github_url = models.URLField(blank=True, max_length=2000)
+    bio = models.TextField(blank=True)
+    twitter_url = models.URLField(blank=True, max_length=2000)
+    website_url = models.URLField(blank=True, max_length=2000)
     photo = models.ImageField(upload_to='users/%Y/%m/%d/', blank=True)
 
     def __str__(self):
