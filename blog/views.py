0f62cb75dae9dd01901df4f77c7440edb2599dee
Peter-Mwangi254 <pm6093628@gmail.com>
2024-05-02 10:40:38 +0300

    added styling on the base template

--- blog/views.py
+++ blog/views.py
@@ -6,19 +6,21 @@ from django.views.decorators.http import require_POST
 # Create your views here.
 def post_list(request):
     """
-    return a list of posts that are published
+    Return a list of posts that are published
     """
     posts = Post.published.all()
-    return render(request, 'blog/post/list.html', {'posts':posts})
+    # Get popular posts
+    popular_posts = Post.published.get_popular_posts()
+    return render(request, 'blog/post/list.html', {'posts': posts, 'popular_posts': popular_posts})
 
 def post_detail(request, id):
     """
-    post detail view
+    Post detail view
     """
     post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
     comments = post.comments.filter(active=True)
     form = CommentForm()
-    return render(request, 'blog/post/detail.html', {'post':post, 'comments': comments, 'form': form})
+    return render(request, 'blog/post/detail.html', {'post': post, 'comments': comments, 'form': form})
 
 @require_POST
 def post_comment(request, post_id):
@@ -31,9 +33,6 @@ def post_comment(request, post_id):
     if form.is_valid():
         comment = form.save(commit=False)
         comment.post = post
-        comment.author_id = 1
+        comment.author_id = 1  # Replace with actual author id
         comment.save()
-    return render(request, 'blog/post/comment.html',
-    {'post': post,
-    'form': form,
-    'comment': comment})
+    return render(request, 'blog/post/comment.html', {'post': post, 'form': form, 'comment': comment})
