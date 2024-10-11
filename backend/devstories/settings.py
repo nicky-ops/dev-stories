0ea1aa6a39c3ca512db3dc2aa3d2cd4fe54bf5d9
Nickson Kipruto Rop <nickyrutto@gmail.com>
2024-10-11 22:42:20 +0300

    configure postgresql

--- backend/devstories/settings.py
+++ backend/devstories/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = "devstories.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "devstoriesdb",
+        "USER": "admin",
+        "PASSWORD": "6*;rz-6Xj71U",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
