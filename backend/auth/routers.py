770e889ef8adea6b6941b673c054532708e46d9b
Nickson Kipruto Rop <nickyrutto@gmail.com>
2024-10-25 19:42:49 +0300

    new routers; login, register, refresh

--- backend/auth/routers.py
+++ backend/auth/routers.py
@@ -1,9 +1,12 @@
 from rest_framework import routers
-from .viewsets import UserViewSet
+from .viewsets import UserViewSet, RegisterViewSet, LoginViewSet, RefreshViewSet
 
 router = routers.SimpleRouter()
 
-router.register(r'users', UserViewSet, basename='user')
+router.register(r'user', UserViewSet, basename='user')
+router.register(r'auth/register', RegisterViewSet, basename='auth-register')
+router.register(r'auth/login', LoginViewSet, basename='auth-login')
+router.register(r'auth/refresh', RefreshViewSet, basename='auth-refresh')
 
 urlpatterns = [
     *router.urls,
